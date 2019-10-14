f0927a8e352b069acf02894dbdf5fa1e871bf2de
Michael <michael.freaney324@gmail.com>
2019-10-14 10:40:29 -0500

    Merging Hitbox Work

--- Assets/Scripts/PotionDisplay.cs
+++ Assets/Scripts/PotionDisplay.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class PotionDisplay : MonoBehaviour
 {
     string[] potion_arr = {"Sulfur", "Mercury", "Salt", "Bomb", "Cyclone", "Cloud"}; //These will be Potion.type
-    int[] potion_quants = {99, 99, 99, 50, 50, 50};//These will be Potion.quantity
+    int[] potion_quants = {0, 0, 0, 0, 0, 0};//These will be Potion.quantity
     Text current_potion;
     int iterator;
     void Awake(){
@@ -25,11 +25,14 @@ public class PotionDisplay : MonoBehaviour
         when LT is pressed: i-- UNLESS i-- == -1, in which case set i to 5.
         
         After any checks for iterator switches -> UI displays potion (text for now) in top left.*/
+
+        //TODO: Skipping system for elements we dont have any of?
         if(Input.GetKeyDown(KeyCode.E)){
             iterator++;
             if(iterator == 6){
                 iterator = 0;
             }
+
         }
         if(Input.GetKeyDown(KeyCode.Q)){
             iterator--;
@@ -39,6 +42,9 @@ public class PotionDisplay : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.Mouse1)){
             potion_quants[iterator] -= 1;
+            if(potion_quants[iterator]<0){
+                potion_quants[iterator] = 0;
+            }
         }
         current_potion.text = potion_arr[iterator]+"\tx"+potion_quants[iterator].ToString();
     }
---
+++ Assets/Scripts/SwordManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BoxCollider sw;
+    public GameObject player;
+    Rigidbody rbp;
+    
+    void Start()
+    {
+        sw = GetComponent<BoxCollider>();
+        player = GameObject.Find("Player");
+        rbp = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Damageable")){
+            Debug.Log("Hit!");
+        }
+    }
+
+
+}
