e91a0e28ab079fb0c8306331449f9bbe1b99ea6e
Mightymaster99 <lessab99@gmail.com>
2019-10-14 10:29:09 -0500

    Merged Frean and Mightymaster99's Projects

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,19 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Alchemy;
 using System.IO;
+using Alchemy;
 
-[System.Serializable]
-public class PotionManager : MonoBehaviour {
+public class EnemyManager : MonoBehaviour {
 
-	[SerializeField]
-	private List<Potion> potions = new List<Potion>();
+	List<Enemy> enemies = new List<Enemy>();
 
-	private void Start() {
-
+    void Start() {
 		// List of elements
-		string path = Directory.GetCurrentDirectory() + "\\potions.txt";
+		string path = Directory.GetCurrentDirectory() + "\\enemies.txt";
 
 		using (var reader = new StreamReader(path)) {
 			// Get reader input
@@ -22,7 +19,7 @@ public class PotionManager : MonoBehaviour {
 			while (input != "" && input != null) {
 				// Creating the items
 				// Create a new potion
-				Potion newPotion = new Potion();
+				Enemy newEnemy = new Enemy();
 				for (int i = -1; i < 7; i++) {
 
 					// Read actual input
@@ -37,36 +34,27 @@ public class PotionManager : MonoBehaviour {
 					switch (i) {
 						case (0):
 							// Setting the element name
-							newPotion.name = input;
+							newEnemy.name = input;
 							break;
 						case (1):
 							// Create the combination
-							newPotion.combination = new List<string>();
-							string[] combo = input.Split(' ');
-							foreach (string j in combo) {
-								newPotion.combination.Add(j);
-							}
+							newEnemy.baseHP = System.Convert.ToInt32(input);
 							break;
 						case (2):
 							// Set up the sprite used
-							newPotion.sprite = (Sprite)Resources.Load(input);
+							newEnemy.baseATK = System.Convert.ToInt32(input);
 							break;
 						case (3):
 							// Set up the maximum size of the explosion
-							newPotion.size = System.Convert.ToInt32(input);
+							newEnemy.baseDEF = System.Convert.ToInt32(input);
 							break;
 						case (4):
 							// Set up the time it lasts
-							newPotion.time = System.Convert.ToInt32(input);
+							newEnemy.speed = System.Convert.ToInt32(input);
 							break;
 						case (5):
 							// Set up the damage it'll do
-							newPotion.damage = System.Convert.ToInt32(input);
-							break;
-						case (6):
-							// Set up the Effect
-							Debug.Log(input);
-							newPotion.effect = input;
+							newEnemy.type = input;
 							break;
 						default:
 							// Just for the -----
@@ -74,17 +62,16 @@ public class PotionManager : MonoBehaviour {
 					}
 				} // End creation
 
-				potions.Add(newPotion);
+				enemies.Add(newEnemy);
 
 			} // End of while (input != "" && input != null)
 		} // End of File IO
 
 		// Removes an empty potion
-		potions.RemoveAt(potions.Count - 1);
+		enemies.RemoveAt(enemies.Count - 1);
 
-		foreach (Potion i in potions) {
+		foreach (Enemy i in enemies) {
 			Debug.Log(i.ToString());
 		}
-
-	}// End of Start
+	}
 }
