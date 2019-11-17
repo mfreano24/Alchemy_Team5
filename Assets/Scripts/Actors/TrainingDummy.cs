01b53dc3ad69f7d8d98f27f7612937ee1fa1f99f
Mightymaster99 <lessab99@gmail.com>
2019-11-17 17:00:36 -0600

    Game Over and Pause Screens

--- Assets/Scripts/Actors/TrainingDummy.cs
+++ Assets/Scripts/Actors/TrainingDummy.cs
@@ -5,32 +5,32 @@ using Alchemy;
 public class TrainingDummy : MonoBehaviour {
 
 	public Enemy thisEnemy;
-	
+
 	GlobalVars gv;
-	
+
 	public GameObject sword;
 	BoxCollider2D hurtbox;
 	Rigidbody2D rb;
-	
+
 	public Vector2 translatePos;
 	public float turnDist = 5;
 	public float turnSpeed = 3;
-	
+
 	Path path;
-	
+
 	const float minPathUpdateTime = 0.1f;
 	const float pathUpdateThreshold = 0.1f;
-	
+
 	int targetIndex;
-		
-	
+
+
 	void Start() {
 		hurtbox = GetComponent<BoxCollider2D>();
 		rb = GetComponent<Rigidbody2D>();
 		StartCoroutine(UpdatePath());
 		gv = GameObject.Find("EventSystem").GetComponent<GlobalVars>();
 	}
-	
+
 	public void OnPathFound(Vector3[] waypoints, bool success) {
 		if (success) {
 			path = new Path(waypoints, transform.position, turnDist);
@@ -38,17 +38,17 @@ public class TrainingDummy : MonoBehaviour {
 			StartCoroutine(Follow());
 		}
 	}
-	
+
 	IEnumerator UpdatePath() {
-	
+
 		if (Time.timeSinceLevelLoad < 0.3f) {
 			yield return new WaitForSeconds(0.3f);
 		}
 		PathRequestManager.RequestPath(new PathRequest(transform.position, GameObject.Find("Player").transform.position, OnPathFound));
-	
+
 		float sqrMoveThreshold = pathUpdateThreshold * pathUpdateThreshold;
 		Vector3 targetPosOld = GameObject.Find("Player").transform.position;
-	
+
 		while (true) {
 			yield return new WaitForSeconds(minPathUpdateTime);
 			if ((GameObject.Find("Player").transform.position - targetPosOld).sqrMagnitude > sqrMoveThreshold) {
@@ -57,11 +57,11 @@ public class TrainingDummy : MonoBehaviour {
 			}
 		}
 	}
-	
+
 	IEnumerator Follow() {
 		bool followingPath = true;
 		int pathIndex = 0;
-	
+
 		while (followingPath) {
 			Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
 			while (path.turnBoundaries[pathIndex].CrossedLine(pos2D)) {
@@ -72,14 +72,14 @@ public class TrainingDummy : MonoBehaviour {
 					pathIndex++;
 				}
 			}
-	
+
 			if (followingPath) {
 				translatePos = path.lookPoints[pathIndex] - transform.position;
 				transform.Translate(translatePos.normalized * Time.deltaTime * thisEnemy.speed, Space.World);
 			}
 
 			yield return null;
-	
+
 		}
 	}
 
@@ -89,14 +89,14 @@ public class TrainingDummy : MonoBehaviour {
 			collision.GetComponent<PlayerController>().Knockback(0.2f, 0.5f, this.transform);
 		}
 	}
-	
+
 	public void DropHealth(int i, bool crit) {
 		if (thisEnemy.baseHP > 0) {
 			GameObject damage = (GameObject)Instantiate(Resources.Load("Damage") as Object, transform.position, Quaternion.identity);
 			// <If possible> damage.transform.GetChild(0).transform.localScale = new Vector3(0.8f / transform.localScale.x, 0.46f / transform.localScale.y, 1); 
 			damage.transform.SetParent(transform);
 			thisEnemy.baseHP -= i;
-	
+
 			if (thisEnemy.baseHP <= 0) {
 				StartCoroutine(IncreaseXP());
 				// Drop the item
@@ -107,12 +107,12 @@ public class TrainingDummy : MonoBehaviour {
 					StartCoroutine(enemyDrop.GetComponent<PotionInstance>().DropPotion());
 				}
 			}
-	
+
 			damage.GetComponent<DamageIndicator>().setHealth(i, crit);
 		}
-	
+
 	}
-	
+
 	public IEnumerator IncreaseXP() {
 		for (int i = 0; i < thisEnemy.exp; i++) {
 			// Animates the increase (because reasons
@@ -122,37 +122,37 @@ public class TrainingDummy : MonoBehaviour {
 	}
 
 
-public void CallKB(float duration, float pow, Transform other){
-	StartCoroutine(Knockback(duration,pow,other));
-}
-
-public IEnumerator Knockback(float duration, float pow, Transform other){
-	float time = 0;
-	while(duration > time){
-		time +=Time.deltaTime;
-		Vector2 direction = (other.transform.position - transform.position).normalized;
-		rb.AddForce(-direction * pow);
+	public void CallKB(float duration, float pow, Transform other) {
+		StartCoroutine(Knockback(duration, pow, other));
 	}
-	yield return 0;
-}
+
+	public IEnumerator Knockback(float duration, float pow, Transform other) {
+		float time = 0;
+		while (duration > time) {
+			time += Time.deltaTime;
+			Vector2 direction = (other.transform.position - transform.position).normalized;
+			rb.AddForce(-direction * pow);
+		}
+		yield return 0;
+	}
 
 
-public void CallSlowDown(float duration, int strength){
-	StartCoroutine(SlowdownDebuff(duration,strength));
-}
-public IEnumerator SlowdownDebuff(float duration, int strength){
-	float tempSpeed = thisEnemy.speed;
-	thisEnemy.speed -= strength;
-	yield return new WaitForSeconds(duration);
-	thisEnemy.speed = tempSpeed;
-	yield return 0;
-}
+	public void CallSlowDown(float duration, int strength) {
+		StartCoroutine(SlowdownDebuff(duration, strength));
+	}
+	public IEnumerator SlowdownDebuff(float duration, int strength) {
+		float tempSpeed = thisEnemy.speed;
+		thisEnemy.speed -= strength;
+		yield return new WaitForSeconds(duration);
+		thisEnemy.speed = tempSpeed;
+		yield return 0;
+	}
 
-//public void OnDrawGizmos() {
-//	if (path != null) {
-//		path.DrawWithGizmos();
-//	}
-//}
+	//public void OnDrawGizmos() {
+	//	if (path != null) {
+	//		path.DrawWithGizmos();
+	//	}
+	//}
 
 }
 
---
+++ Assets/Scripts/UI/Gameover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameover : MonoBehaviour {
+
+	bool tryAgain;
+
+	private void Start() {
+		tryAgain = true;
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) {
+			tryAgain = !tryAgain;
+			GameObject.Find("Cursor").transform.localPosition = new Vector3(-100, tryAgain ? -50 : -150);
+		}
+
+		if (Input.GetButton("Submit")) {
+			if (tryAgain) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene("MainRoom");
+			} else {
+				UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+			}
+		}
+	}
+
+}
