3b2caa7ccb29e5e15befe8a067a55d62cabaa700
Mightymaster99 <lessab99@gmail.com>
2019-11-19 13:02:03 -0600

    Made a Main Menu (And deleted unused scenes)

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,13 +1,79 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
 
+	GameObject title;
+
+	int selectedIndex = 0;
+
+	int MAX_OBJECTS = 2;
+
+	private void Start() {
+		title = GameObject.Find("Title");
+		StartCoroutine(Pulse());
+	}
+
 	// Update is called once per frame
 	void Update() {
 		if (Input.GetButtonDown("Submit")) {
-			UnityEngine.SceneManagement.SceneManager.LoadScene("MainRoom");
+			
+		}
+
+		if (Input.GetKeyDown(KeyCode.S)) {
+			selectedIndex++;
+			if (MAX_OBJECTS < selectedIndex) {
+				selectedIndex = 0;
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.W)) {
+			selectedIndex--;
+			if (selectedIndex < 0) {
+				selectedIndex = MAX_OBJECTS;
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			SelectObject();
+			return;
+		}
+		UpdatePos();
+	}
+
+	private void UpdatePos() {
+		GameObject.Find("Cursor").transform.localPosition = new Vector3(-75, -150 * selectedIndex, 0);
+	}
+
+	private void SelectObject() {
+		switch (selectedIndex) {
+
+			case 0:
+				UnityEngine.SceneManagement.SceneManager.LoadScene("MainRoom");
+				break;
+			case 1:
+				Options();
+				break;
+			case 2:
+				Application.Quit();
+				break;
+		}
+	}
+
+	private void Options() {
+		throw new NotImplementedException();
+	}
+
+	float f(int x) {
+		return 0.3f * Mathf.Sin(Time.frameCount * Mathf.PI / 180) + 1;
+	}
+
+	IEnumerator Pulse() {
+		int x = 0;
+		while (true) {
+			title.transform.localScale = Vector3.one * (0.1f * Mathf.Sin(x / 30f) + 0.85f);
+			x++;
+			yield return new WaitForSeconds(0.01f);
 		}
 	}
 }
