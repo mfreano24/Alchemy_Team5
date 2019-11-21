5f322a0bb365c50ff5835a04de1de76f6caf62d5
Mightymaster99 <lessab99@gmail.com>
2019-11-21 17:54:57 -0600

    Random Sulfur

---
+++ Assets/ElementSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementSpawner : MonoBehaviour {
+	public int timer;
+
+	PotionManager pm;
+
+	public void Start() {
+		pm = GameObject.Find("EventSystem").GetComponent<PotionManager>();
+		StartCoroutine(Spawner());
+	}
+
+	IEnumerator Spawner() {
+		while (true) {
+
+			timer = Random.Range(900, 2400);
+
+			for (int i = 0; i < timer; i++) {
+				yield return new WaitForEndOfFrame();
+			}
+
+			GameObject enemyDrop = (GameObject)Instantiate(GameObject.Find("Player").GetComponent<PlayerController>().potionPrefab, transform.position, Quaternion.identity);
+			enemyDrop.GetComponent<PotionInstance>().thisPotion = pm.potions[0];
+			enemyDrop.GetComponent<PotionInstance>().isEnemyDrop = true;
+			StartCoroutine(enemyDrop.GetComponent<PotionInstance>().DropPotion());
+		}
+	}
+}
